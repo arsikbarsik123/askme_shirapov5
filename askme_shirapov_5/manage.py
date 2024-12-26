c1af608dee0a5eeb6937ff931d7f98ea640d2101
Arsalan <batyavzdanii749@gmail.com>
2024-12-26 22:24:57 +0300

    lasdb

--- askme_shirapov_5/manage.py
+++ askme_shirapov_5/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AskPupkin_Makarenkov.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'askme_shirapov_5.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
